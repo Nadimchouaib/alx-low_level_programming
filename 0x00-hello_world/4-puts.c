c5881984346cf9983032bdafa970edbbdadea272
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-07 06:24:15 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main : this is a simple main Function to print a specific sentence
+ *main - this is a simple main Function to print a specific sentence
  *
- *Return : always 0
+ *Return: always 0
  */
 
 int main(void)
