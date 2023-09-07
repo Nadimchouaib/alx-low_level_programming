cd8d0b370d92ee8139ef8b9a44e2559ef68a6a62
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-07 06:38:36 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  *main -  This function prints a piece of art sentence
  *Return: The main function returns 0
- */
+*/
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
