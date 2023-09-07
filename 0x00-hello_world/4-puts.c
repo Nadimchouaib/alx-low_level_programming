6800565644760072ff6a6e3b7214e9d8a7885c68
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-07 06:13:29 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void) 
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+/**
+ *This is a simple main Function to print a specific sentence
+ *Return 0
+ */
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
