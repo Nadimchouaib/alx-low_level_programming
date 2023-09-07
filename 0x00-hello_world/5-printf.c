e27014192c9fa2664a702b5a36c40ece33ed1245
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-07 06:30:04 -0700

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** Description: This program prints a piece of art sentence
+ *
+ *Return: The main function returns 0
+ */
+
+int maini(void) {
+    printf("This is a piece of art, followed by a new line.\n");
+    return (0);
+}
