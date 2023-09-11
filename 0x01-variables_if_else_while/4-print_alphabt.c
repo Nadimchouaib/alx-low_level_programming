0028de70bb4c4e825a8512cfd60874c83c5c65aa
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-11 11:31:45 -0700

    Task 4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet 
+ * main - Prints the alphabet
  *
  * Return: Always 0 
  */
@@ -8,6 +8,7 @@
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
