2c3576fa2e32816a175fabe9000e15545185842a
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-11 16:39:39 -0700

    Task 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10 )
+		{
+			putchar (i + '0');
+		}
+		else
+		{
+			putchar('a' + i - 10);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
