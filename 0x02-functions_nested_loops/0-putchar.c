2b23686c8f7bf928613fa5c28626ed74e0c12ef4
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-12 08:03:55 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char Char[] = "_putchar\n";
+	int i = 0;
+
+	while (Char[i] != '\0')
+	{
+		putchar(Char[i]);
+		i++;
+	}
+
+	return (0);
+}
