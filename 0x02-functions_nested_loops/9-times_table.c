218065ec1834945aa76eb2baa7de2c9df1d2c975
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-12 16:20:56 -0700

    Task 9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,18 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (result < 10)
+
+			if (result < 10 && j != 0)
 			{
 				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+			}
+			if (result < 10)
+			{
 				_putchar('0' + result);
 			}
 			else
@@ -26,7 +35,6 @@ void times_table(void)
 			if (j < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
