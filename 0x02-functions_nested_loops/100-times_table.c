269a4a3fd34926cbb5758e685d233c21a48c7c82
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-12 19:39:33 -0700

    Task 13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table.
+ */
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+
+			if (result < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+			}
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (j < n)
+			{
+				_putchar(',');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
