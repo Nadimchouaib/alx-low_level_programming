b46f1bf75a3e7cef70fb33249329b233dc8b58fa
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-13 17:37:02 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,51 +10,51 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
 
-			if (result < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (result > 100 && j != 0)
-			{
-				_putchar(' ');
-			}
-			else if (j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			if (result < 10)
-			{
-				_putchar('0' + result);
-			}
-			else if (result >= 100)
-			{
-				_putchar('0' + result / 100);
-				_putchar('0' + (result / 100) / 10);
-				_putchar('0' + (result / 100) % 10);
-			}
-			else
-			{
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			if (j < n)
-			{
-				_putchar(',');
-			}
-			else
-			{
-				_putchar('\n');
+				if (result < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (result > 99 && j != 0)
+				{
+					_putchar(' ');
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (result < 10)
+				{
+					_putchar('0' + result);
+				}
+				else if (result >= 100)
+				{
+					_putchar('0' + result / 100);
+					_putchar('0' + (result % 100) / 10);
+					_putchar('0' + (result % 100) % 10);
+				}
+				else
+				{
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
+				}
+				if (j < n)
+				{
+					_putchar(',');
+				}
+				else
+				{
+					_putchar('\n');
+				}
 			}
 		}
-	}
 	}
 }
