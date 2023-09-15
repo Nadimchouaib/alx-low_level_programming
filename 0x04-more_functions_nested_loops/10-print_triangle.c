97f44adf5c5f719932fd7e6dc2d6023f8d14f29d
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-14 18:46:17 -0700

    Task 10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - function that prints a triangle
- *@size:  the size of the triangle
+ *@size : the size of the triangle
  */
 
 void print_triangle(int size)
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size - i; j++)
 			{
