77e532c91ab643430cf19885fdd6b6dc27ff0696
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-14 17:02:46 -0700

    Task 5. Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints the numbers, from 0 to 9
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+			}
+			else
+			{
+				_putchar('0' + i);
+			}
+		}
+		_putchar('\n');
+	}
+}
