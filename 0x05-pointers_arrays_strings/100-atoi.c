70fe8dce6d4f219153aeabd56656404d43e15e39
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-19 17:53:49 -0700

    Task 6

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,10 +3,30 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- * Return: 0 or the int converted
+ * Return: result  or the int converted
  */
 
 int _atoi(char *s)
 {
+	int i = 0;
+	int sign = 1;
+	int result = 0;
 
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
