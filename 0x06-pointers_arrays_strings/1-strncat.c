30d5c1f1c1828739868651173863056d27f2a542
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-21 16:53:13 -0700

    Task 1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strnca -  function that concatenates two strings
+ * @dest: input value1
+ * @src: input value2
+ * @n: input value3
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
