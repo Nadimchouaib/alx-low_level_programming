bf091d7616a821b3ba165f760e505b452657e7a7
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-28 09:56:00 -0700

    Task 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function to revers a string
+ * @s: par1
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
