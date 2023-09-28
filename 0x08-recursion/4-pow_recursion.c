9474b33dac78008e04b610062443ef06dcf14cb3
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-28 10:21:19 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns x power of y
+ * @x: par1
+ * @y: par2
+ * Return: x power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+
+}
