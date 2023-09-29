fa916a495a7190077c8af6302d863207b1ba3faf
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-09-28 17:21:50 -0700

    Task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int check_if_pal(char *s, int i, int length);
+int lengh_recursion(char *s);
+
+/**
+ * is_palindrome - function to checks if a string is a palindrome
+ * @s: string parameter to be reversed
+ * Return: 1 if it is, 0 it's not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s)
+		return (1);
+	return (check_if_pal(s, 0, lengh_recursion(s)));
+}
+
+/**
+ * lengh_recursion - function that returns the length of a string
+ * @s: string parameter
+ * Return: length of the string
+ */
+
+int lengh_recursion(char *s)
+{
+	if (*s)
+		return (0);
+	return (1 + lengh_recursion(s + 1));
+}
+
+/**
+ * check_if_pal - function to checks the characters
+ * recursively for palindrome
+ * @s: string parameter
+ * @i: iterator parameter
+ * @length: length parameter of the string
+ * Return: 1 if palindrome, 0 if not palindrome
+ */
+
+int check_if_pal(char *s, int i, int length)
+{
+	if (*(s + i) != *(s + length - 1))
+		return (0);
+	if (i >= length)
+		return (1);
+	return (check_if_pal(s, i + 1, length - 1));
+}
