86463a9b78e5842318f651105cb2df1bac2179da
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-11 18:35:19 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
  * print_name - print name using pointer to function
  * @name: name to be printed
  * @f: pointer to function
- * Return: nothing
+ * Return: 1 if name is null
  **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		return;
+		return (1);
 	f(name);
 }
