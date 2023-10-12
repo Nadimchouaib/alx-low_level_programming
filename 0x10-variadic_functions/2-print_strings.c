f3082a8037195d765fd2b6686d9e9821d0519234
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-12 16:47:21 -0700

    Tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,15 +5,15 @@
  * @separator: separator between strings
  * @n: number of arguments
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	char *str;
 
-	va_list list;
+	va_list arg;
 
 	va_start(arg, n);
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(arg, char *);
@@ -26,6 +26,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s%s", separator, str);
 	}
+
 	printf("\n");
+
 	va_end(arg);
 }
