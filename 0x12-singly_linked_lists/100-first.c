9065ccf63b152f0d031c4b5fad77d180fd8df16c
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-19 19:24:22 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_before_main - Function to print the message before the main function
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
