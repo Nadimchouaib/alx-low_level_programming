4d4e27d9515707437ac0e2e0e2622d2e2fc32e10
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-19 18:22:17 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int nd_count = 0;
+	size_t nd_count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
+		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
 		nd_count++;
 	}
