33e67ea782a6cd7f5d8786892789d080a1602673
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-19 19:10:53 -0700

    Task 3

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - function that free a list_t list
+ * @head: A pointer to the beginning of the list_t list
+ */
+
+void free_list(list_t *head)
+{
+  	  list_t *current_node;
+
+  	 while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
