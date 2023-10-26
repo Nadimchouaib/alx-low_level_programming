82955a96ee84c18bfb54262875e583ef8eddc340
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-25 18:34:16 -0700

    Task 102

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,7 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 listint_t *reverse_listint(listint_t **head);
+
+int is_in_list(listnode_t *head, const listint_t *ptr)
+listnode_t *add_node(listnode_t **head, const listint_t *ptr);
 size_t print_listint_safe(const listint_t *head);
+
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
