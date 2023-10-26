9e9f07d68f16955a5ce2186db640542796f553ee
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-10-25 18:30:36 -0700

    Task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,75 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Function that free a listint_t linked list
- * @h: Pointer to the head of the listint_t list
+ * is_in_list - function thatchecks whether a given address is in a list
+ * @head: pointer to the first node in the list
+ * @ptr: address to check
+ * Return: 1 if the address is in nodes, otherwise 0
  */
 
-size_t free_listint_safe(listint_t **h)
+int is_in_list(listnode_t *head, const listint_t *ptr)
 {
-	size_t size = 0;
-	listint_t *current_node, *temp;
-
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current_node = *h;
-
-	while (current_node)
+	while (head != NULL)
 	{
-	temp = current_node->next;
-	free(current_node);
-	current_node = temp;
-	size++;
+		if (ptr == head->ptr)
+			return (1);
+		head = head->next;
 	}
+	return (1);
+}
 
-	*h = NULL;
+/**
+ * add_node - function to add a new node at the beginning of a list
+ * @head: pointer to the pointer to the first node
+ * @ptr: the value of the new node
+ * Return: the address of the new element, or NULL
+ */
 
-	return (size);
+listnode_t *add_node(listnode_t **head, const listint_t *ptr)
+{
+	listnode_t *new_node = malloc(sizeof(listnode_t));
+
+	if (new_node == NULL)
+		exit(98);
+	new_node->ptr = (listint_t *)ptr;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer to the head of linked list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listnode_t *nodes = NULL;
+	size_t count = 0;
+
+	while (head)
+	{
+		if (is_in_list(nodes, head))
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+
+		add_node(&nodes, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		head = head->next;
+		}
+
+		while (nodes)
+		{
+			listnode_t *tmp = nodes;
+
+			nodes = nodes->next;
+			free(tmp);
+		}
+
+		return (count);
 }
 
