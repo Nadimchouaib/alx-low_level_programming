05c5fc0d8f8efab7b27fad964ad13b6ea85e9066
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-11-03 15:46:54 -0700

    Task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,11 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
+int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
