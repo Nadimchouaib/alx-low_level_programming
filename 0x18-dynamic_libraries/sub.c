18225c41aba48ba133276342021d403b6a7cd793
Nadimchouaib <ndm.bouchaib@gmail.com>
2023-12-15 16:34:55 -0800

    Task 3

---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * sub - Subtracts two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The result of the subtraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
